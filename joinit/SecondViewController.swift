7956326c416593ab8cbb65dd330c56801004005a
Hamaad Markhiani <hamaad.markhiani@gmail.com>
2016-02-10 02:02:26 -0600

    Initial Commit

---
+++ joinit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  joinit
+//
+//  Created by Hamaad Markhiani on 2/10/16.
+//  Copyright © 2016 HamAndCheese. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
