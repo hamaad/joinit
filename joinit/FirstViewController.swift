38f56fdd33289dbe5bdf6b5f2a771f58da695d43
Hamaad Markhiani <hamaad.markhiani@gmail.com>
2016-02-11 21:48:40 -0600

    wrote ham

--- joinit/FirstViewController.swift
+++ joinit/FirstViewController.swift
@@ -3,7 +3,7 @@
 //  joinit
 //
 //  Created by Hamaad Markhiani and Christian Galang on 2/10/16.
-//  Another comment 23456
+//  Another comment ham
 //  Copyright © 2016 HamAndCheese. All rights reserved.
 //
 
