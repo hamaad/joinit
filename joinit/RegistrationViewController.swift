432496f654a27d46e4e3a68e6b361cd247c71952
Hamaad Markhiani <hamaad.markhiani@gmail.com>
2016-02-13 17:53:45 -0600

    Finished registration and added a basic error message.

--- joinit/RegistrationViewController.swift
+++ joinit/RegistrationViewController.swift
@@ -7,32 +7,33 @@
 //
 
 import UIKit
-
 import Firebase
 
 
 class RegistrationViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!     // Establish a variable for the email address text field
+    @IBOutlet weak var resultLabel: UILabel! // Where to display text if there was an error with login
     @IBOutlet weak var passwordTextField: UITextField!  // Establish a variable for the password text field
     
     var databaseConnection = Firebase(url: "https://sizzling-fire-4884.firebaseio.com/") // Establish our database connection and assign it to an variable (object???)
     
-    /*@IBAction func registerButton(sender: UIButton) {   // Function that is called when user hits the register button
-        databaseConnection.createUser( emailTextField.text, password: passwordTextField.text,
-            withCompletionBlock: { error, result in
-            
-                if error != nil {
-                    // There was an error creating the account
-                } else {
-                    let uid = result["uid"] as? String
-                    print("Successfully created user account with uid: \(uid)")
-                }
-        
-        }
-        )
-    }*/
     
+    @IBAction func registerButton(sender: UIButton) {
+        // Function that is called when user hits the register button
+        databaseConnection.createUser(emailTextField.text, password: passwordTextField.text,
+            withValueCompletionBlock: { error, result in
+                
+                if error != nil {
+                    self.resultLabel.text = "Registration unsuccessful. Invalid email or email registered already."
+                } else {
+                    self.resultLabel.text = "Registration successful! Sending you to login whenever it's made."
+                    // TODO: Send to login screen.
+                }
+                
+            }
+        )
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
