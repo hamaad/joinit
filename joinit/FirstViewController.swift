8e4fa0277bf9dff6ef4c17c323aaa437a63b07f3
Hamaad Markhiani <hamaad.markhiani@gmail.com>
2016-02-22 12:32:14 -0600

    Made the first view's UI functional. Temporary.

--- joinit/FirstViewController.swift
+++ joinit/FirstViewController.swift
@@ -15,17 +15,15 @@ class FirstViewController: UIViewController {
     
     var myRootRef = Firebase(url: "https://sizzling-fire-4884.firebaseio.com/")
     // Create a reference to a Firebase location
+
+    @IBOutlet weak var messageTextField: UITextField!   // create a variable for the message text field
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.blackColor()], forState: .Normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.blackColor()], forState: .Normal)    // Make the navigation TAB bar a custom color: black
         
-        myRootRef.observeEventType(.Value, withBlock: {
-            snapshot in
-            print("\(snapshot.key) -> \(snapshot.value)")
-        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +31,9 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func sendMessageButton(sender: UIButton) {    // called when the send message button is pressed
+        sendMsg(messageTextField.text!)
+    }
     func sendMsg(message:String)
     {
         myRootRef.setValue(message) // Write data to my Firebase database
